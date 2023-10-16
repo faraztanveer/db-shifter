e7eb060844b518c952c139ba6e1f9791e44ad412
Faraz tanveer <faraztanveer.dev@gmail.com>
2023-10-16 17:04:05 +0500

    commands , get current db  and setdefaultdb

--- src/MultiDBServiceProvider.php
+++ src/MultiDBServiceProvider.php
@@ -1,18 +1,30 @@
 <?php
+
 namespace MultiDB;
+
 use Illuminate\Support\ServiceProvider;
 
-class MultiDbServiceProvider extends ServiceProvider 
+class MultiDbServiceProvider extends ServiceProvider
 {
     /**
      * Register any package services.
      *
      * @return void
      */
-    public function register() 
+    public function register()
     {
         $this->app->singleton('multidb', function ($app) {
             return new Database\DatabaseShifter($app['config'], $app['db']);
         });
+    }
+
+
+    protected function registerCommands()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Console\Commands\MultiDbMigrateCommand::class,
+            ]);
+        }
     }
 }
